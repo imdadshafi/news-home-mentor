be4eb30a868717e3dcd3881aff85d8b3145df264
imdad shafi <imdadshafi@gmail.com>
2024-03-07 19:42:44 +0500

    responsive

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-import './style.scss'
+import './app.scss'
 
 const menuIcon = document.querySelector('.menu-icon')
 const nav = document.querySelector('nav')
+const navWrap = document.querySelector('.nav-wrapper')
 if (menuIcon) {
   menuIcon.addEventListener('click', function () {
-    menuIcon.classList.toggle('opened');
+    menuIcon.classList.toggle('opened')
     nav.classList.toggle('active')
   })
 }
+nav.addEventListener('click', e => {
+  if (e.target.contains(nav)) {
+    menuIcon.classList.remove('opened')
+    nav.classList.remove('active')
+  }
+})
